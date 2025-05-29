2c1db1a410aa19da981652733c6ca46377c06d3c
a7mad1112 <ahmalawneh79@gmail.com>
2025-05-29 23:01:47 +0300

    define id generator function

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,22 @@
-const fakeData = [
-  {
-    title: "welcome to todo task manager",
-    completed: true,
-    id: 1
+function* idGenerator() {
+  let counter = 1;
+  while (true) {
+    yield counter++;
   }
-]
+}
+
+const idGen = idGenerator();
+const fakeData = [];
+
+fakeData.push({
+  title: "welcome to todo task manager",
+  completed: true,
+  id: idGen.next().value,
+});
+fakeData.push({
+  title: "welcome to todo task manager",
+  completed: true,
+  id: idGen.next().value,
+});
+
+console.log(fakeData);
